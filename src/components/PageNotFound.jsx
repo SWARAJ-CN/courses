255c52d291b8699824935fe1e8972cfba7b5b742
swaraj cn <swarajcn774@gmail.com>
2026-05-31 17:17:40 +0530

    404added

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -6,7 +6,7 @@ const PageNotFound = () => {
     <>
     <div className='h-full w-full p-10 flex flex-col items-center justify-center gap-5'>
         <img src={assets.opps} alt='..' className='max-w-xl'/>
-        <p className='text-5xl font-extrabold md:text-9xl'>404</p>
+        <p className='text-7xl font-extrabold md:text-9xl'>404</p>
         <p className='text-center font-base '>We can't seem to find the page you are looking for. It might have been removed, had its name changed, or is temporarily unavailable.</p>
     </div>
     </>
